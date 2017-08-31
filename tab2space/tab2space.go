ca7a4d71311838acfa7da7fe89248b0fc5c5c269
Algoru <alvarostagg@protonmail.com>
2017-08-31 12:15:00 -0500

    Update email

--- tab2space/tab2space.go
+++ tab2space/tab2space.go
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Alvaro Stagg [alvarostagg@openmailbox.org]
+ * Copyright 2017 Alvaro Stagg [alvarostagg@protonmail.com]
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
